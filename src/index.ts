d4862b0a5285165c3017584c3a33a6b71744a448
austin4idps <austin@4idps.com>
2023-10-17 18:35:43 +0800

    feat: support multi-attr

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,80 @@
 import * as dotenv from "dotenv";
 import { writeFileSync } from "fs";
-import random from "random";
 dotenv.config();
 
 function main() {
-  const attributeKey = process.env.ATTRIBUE_KEY;
-  const attribueValue = process.env.ATTRIBUE_VALUE!.split(","); // [a,b,c]
-  const attributesValueCount = process.env
-    .ATTRIBUE_VALUE_COUNT!.split(",")
-    .map((count) => Number(count)); // [10, 11, 12]
+  const attributeKeyArray: string[] = [];
+  const attributeValueArray: string[][] = [];
+  const keyAmount = Number(process.env.KEY_AMOUNT) || 0;
+  function genMetadata() {
+    // start from 1
+    for (let nums = 1; nums <= keyAmount; nums++) {
+      const currentKey = process.env[`ATTRIBUTE_KEY${nums}`];
+      if (!!currentKey) {
+        const values = process.env[`ATTRIBUTE_VALUE${nums}`]?.split(",");
+        const count4Values =
+          process.env[`ATTRIBUTE_VALUE_COUNT${nums}`]?.split(",");
+        const valueArray = [];
 
-  // init record counting map
-  const attrubeMap: Map<string, number> = new Map();
-
-  // set the map
-  for (let i = 0; i < attribueValue.length; i++) {
-    attrubeMap.set(attribueValue[i], attributesValueCount[i]);
-  }
-  const nftCount = Number(process.env.COUNT);
-
-  console.log("nft count:", nftCount);
-  let count = 0;
-
-  while (count < nftCount) {
-    const metas = `{
-      "name" : "${process.env.NAME} #${count}",
-      "description" : "${process.env.DESC}",
-      "image" : "${process.env.IPFS}/${count}.png",
-      "attributes": {
-        "trait_type": "${attributeKey}",
-        "value": "${randomPickValue(attrubeMap)}"
+        if (values?.length && count4Values?.length) {
+          for (let j = 0; j < values.length; j++) {
+            const arraySegment = new Array(Number(count4Values[j])).fill(
+              values[j]
+            );
+            valueArray.push(...arraySegment);
+          }
+        }
+        attributeKeyArray.push(currentKey);
+        attributeValueArray.push(valueArray);
       }
-    }`;
+    }
 
-    const json = JSON.parse(metas);
-    writeFileSync(
-      `genericJson/${count + ".json"}`,
-      JSON.stringify(json, null, 2)
-    );
-    count++;
-  }
-}
+    const nftCount = Number(process.env.COUNT) || 0;
 
-function randomPickValue(map: Map<string, number>) {
-  const keys: string[] = Array.from(map.keys());
-  const randomInt = random.int(0, keys.length - 1);
-  const value = keys[randomInt];
-  let count = map.get(value)!;
-  if (count - 1 === 0) {
-    map.delete(value);
-  } else {
-    map.set(value, count - 1);
+    console.log("NFT count:", nftCount);
+    // token Id will start from 0
+    let tokenId = 0;
+
+    while (tokenId < nftCount) {
+      const metas = `{
+        "name" : "${process.env.NAME} #${tokenId}",
+        "description" : "${process.env.DESC}",
+        "image" : "${process.env.IPFS}/${tokenId}.png",
+        "attributes": ${genAttributes(tokenId)}
+        }`;
+
+      console.log(`${tokenId}:`, metas);
+      const json = JSON.parse(metas);
+
+      writeFileSync(
+        `genericJson/${tokenId + ".json"}`,
+        JSON.stringify(json, null, 2) // parse json with neat padding
+      );
+      tokenId++;
+    }
   }
-  return value;
+
+  function genAttributes(count: number) {
+    let result = ``;
+    let attrCount = 0; // counting attrbuites  inside result
+    for (let i = 0; i < attributeKeyArray.length; i++) {
+      if (!!attributeValueArray[i][count]) {
+        result += `${attrCount > 0 ? "," : ""}{
+        "trait_type": "${attributeKeyArray[i]}",
+        "value": "${attributeValueArray[i][count]}"
+      }`;
+
+        attrCount++;
+      }
+    }
+    if (attrCount >= 2) {
+      return `[${result}]`;
+    } else {
+      return result;
+    }
+  }
+
+  genMetadata();
 }
 
 main();
